4c42ec74382fbaeec6f0caa12ac5be21a9ddaa89
Michał Buszkiewicz <buszkiewiczm@gmail.com>
2019-12-02 13:35:02 +0100

    Add specs for additional schema, column example rendering, and attribute keys

---
+++ spec/support/serializers/foo_serializer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class FooSerializer
+  include FastJsonapi::ObjectSerializer
+
+  belongs_to :bar, key: :foo_bar
+  has_one :fizz, key: :foo_fizz
+  has_many :buzzes, key: :foo_buzzes
+
+  attribute :xyz
+
+  def self.additional_schema
+    {
+      data: {
+        example: {
+          attributes: {
+            xyz: 'foobar'
+          }
+        }
+      }
+    }
+  end
+end
